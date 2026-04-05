3f5db46e31d3f63fc7f38d07b1227a1e29e344eb
StanR <hi@stanr.info>
2026-04-05 19:57:23 +0500

    buff agil for some reason

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -32,7 +32,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private double currentStrain;
 
         private double skillMultiplierSnap => 70.9;
-        private double skillMultiplierAgility => 2.35;
+        private double skillMultiplierAgility => 2.4;
         private double skillMultiplierFlow => 243.0;
         private double skillMultiplierTotal => 1.121;
         private double combinedSnapNormExponent => 1.2;

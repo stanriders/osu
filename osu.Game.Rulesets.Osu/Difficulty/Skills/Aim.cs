c80fc879d222d2d34782192f7b8eb62f42933af5
StanR <hi@stanr.info>
2026-06-20 23:12:52 +0500

    Reintroduce velocity change bonus but lighter

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -31,9 +31,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double currentStrain;
 
-        private double skillMultiplierSnap => 71.9;
+        private double skillMultiplierSnap => 71.2;
         private double skillMultiplierAgility => 2.35;
-        private double skillMultiplierFlow => 242.0;
+        private double skillMultiplierFlow => 240.0;
         private double skillMultiplierTotal => 1.12;
         private double combinedSnapNormExponent => 1.2;
 

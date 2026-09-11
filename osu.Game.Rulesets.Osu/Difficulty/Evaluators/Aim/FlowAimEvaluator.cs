a61afcc0bcb1fd2cf251b7e62ca97dbb211dc0ef
StanR <hi@stanr.info>
2026-09-12 00:34:19 +0500

    Adjust flow evaluator's angular velocity bonus

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/FlowAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/FlowAimEvaluator.cs
@@ -62,7 +62,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
                 double angularVelocity = angleDifferenceAdjusted / (osuCurrObj.AdjustedDeltaTime * 0.1);
 
                 // Low angular velocity flow (angles are consistent) is easier to follow than erratic flow
-                flowDifficulty *= 0.8 + Math.Sqrt(angularVelocity / 270.0);
+                flowDifficulty *= 0.78 + Math.Log(1 + angularVelocity / 8.0, 40);
             }
 
             if (osuCurrObj.Angle != null && osuNextObj?.Angle != null)

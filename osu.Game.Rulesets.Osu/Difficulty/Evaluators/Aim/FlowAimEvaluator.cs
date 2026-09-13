942f9d33bdbd0f6bd849eb3aa2ea95ec26b61db2
StanR <hi@stanr.info>
2026-09-13 20:22:28 +0500

    Flowering night

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/FlowAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/FlowAimEvaluator.cs
@@ -27,7 +27,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
 
             const double velocity_change_multiplier = 0.5;
             const double rhythm_change_cap = 0.1;
-            const double acute_angle_multiplier = 1.0;
+            const double acute_angle_multiplier = 0.9;
 
             var osuLastLastObj = (OsuDifficultyHitObject)current.Previous(1);
 
--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -57,8 +57,8 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private double calculateAdjustedDifficulty(DifficultyHitObject current)
         {
             const double skill_multiplier_snap = 73.5;
-            const double skill_multiplier_agility = 167.0;
-            const double skill_multiplier_flow = 248.5;
+            const double skill_multiplier_agility = 170.0;
+            const double skill_multiplier_flow = 250.0;
 
             double snapDifficulty = SnapAimEvaluator.EvaluateDifficultyOf(current, IncludeSliders) * skill_multiplier_snap;
             double agilityDifficulty = AgilityEvaluator.EvaluateDifficultyOf(current) * skill_multiplier_agility;
